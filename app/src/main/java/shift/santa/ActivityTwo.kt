2047bfaae91e38e94cffa578614945230a89093f
AleksKirienko <aleks.Kirienko@bk.ru>
2020-01-28 18:25:50 +0700

    Add new activity: ActivityTwo

---
+++ app/src/main/java/shift/santa/ActivityTwo.kt
@@ -0,0 +1,12 @@
+package shift.santa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityTwo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_two)
+    }
+}
--- app/src/main/java/shift/santa/MainActivity.kt
+++ app/src/main/java/shift/santa/MainActivity.kt
@@ -1,12 +1,27 @@
 package shift.santa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_two.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Button1.setOnClickListener {
+            startActivity(Intent(this, ActivityTwo::class.java))
+        }
     }
+
+    /*fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello", Toast.LENGTH_SHORT)
+
+        myToast.show()
+    }*/
 }
